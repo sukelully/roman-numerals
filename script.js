80c2057b4a14713ff6a9e63a7e02af15f30de03c
sukelully <sully097@gmail.com>
2024-09-04 19:14:10 +0100

    Think this is the right direction

--- script.js
+++ script.js
@@ -2,31 +2,48 @@ const inputNum = document.getElementById('number');
 const btn = document.getElementById('convert-btn');
 const output = document.getElementById('output');
 
-const numArr = [];
+let inputArr;
+const resultArr = [];
 
 const convert = (num) => {
-    console.log(num);
     if (num === 0) {
         console.log('loop finished');
         return;
-    } else if (num > 3) {
-
+    } else if (num < 4) {
+        resultArr.push('I');
+        convert(num-1);
     }
 }
 
-const parseInput = (num) => {
+const checkInput = (num) => {
     if (num < 1) {
-        output.innerText = "Please enter a number greater than or equal to 1"
+        output.innerText = "Please enter a number greater than or equal to 1";
+        console.log("Please enter a number greater than or equal to 1");
         return true;
+    } else if (num >= 4000) {
+        output.innerText = "Please enter a number less than or equal to 3999";
+        console.log("Please enter a number less than or equal to 3999");
+        return true;
+    }
+    inputArr = num.split('').reverse();;
+    updateInputArray();
+}
+
+const updateInputArray = () => {
+    let increment = 1;
+    for (let i = 0; i < inputArr.length; i++) {
+        inputArr[i] *= increment;
+        console.log(inputArr[i]);
+        increment *= 10;
     }
 }
 
 const updateOutput = () => {
-    if (parseInput(inputNum.value)) {
+    if (checkInput(inputNum.value)) {
         return;
     }
-    convert();
-    output.innerText = inputNum.value;
+    convert(inputNum.value);
+    output.innerText = resultArr.join('');
 }
 
 btn.addEventListener('click', updateOutput);
